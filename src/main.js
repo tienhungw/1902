1a362eac7a06fb69f6cccdddb4ed1dd4660f4c97
tienhungw <nguyentienhung04042004@gmail.com>
2025-02-20 00:27:11 +0700

    20022025

--- src/main.js
+++ src/main.js
@@ -1,17 +1,38 @@
-
 import Navigo from "navigo";
-const router = new Navigo("/", { hash: true });
+const router = new Navigo("/");
 
-import Trangchu, { Kenhdangky, Shorts } from "./script.js";
 
-router.on("/", () => {
-    Trangchu();
+const content = document.getElementById("content");
+
+const Trangchu = () =>{
+    return `
+    <h2 class="text-2xl font-bold">Chế độ lưu danh sách video đã xem đang tắt</h2>
+    `;
+};
+
+const Shorts = () =>{
+    return `
+    <h2 class="text-2xl font-bold">Short của bạn</h2>
+    `;
+};
+
+const Kenhdangky = () =>{
+    return `
+    <h2 class="text-2xl font-bold">Đây là danh sách các kênh đăng ký</h2>
+    `;
+};
+
+
+router.on("/",() => {
+    content.innerHTML = Trangchu();
 });
-router.on("/shorts", () => {
-    Shorts();
+
+router.on("/shorts",() => {
+    content.innerHTML = Shorts();
 });
-router.on("/feed", () => {
-    Kenhdangky();
+
+router.on("/feed",() => {
+    content.innerHTML = Kenhdangky();
 });
 
 router.resolve();
